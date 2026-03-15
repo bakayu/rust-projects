f84ebb2fc0afaf9c10cf134ed2f9e1416b3fbcc3
Ayush <mail@ayuch.dev>
2026-03-15 17:48:25 +0530

    feat: shared state between tasks
    Signed-off-by: Ayush <mail@ayuch.dev>

--- tokio/mini-redis/src/main.rs
+++ tokio/mini-redis/src/main.rs
@@ -1,35 +1,27 @@
 use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
+use bytes::Bytes;
 use mini_redis::Command::{self, Get, Set};
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
-#[tokio::main]
-async fn main() {
-    let listner = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+type ShardedDb = Arc<Mutex<HashMap<String, Bytes>>>;
 
-    loop {
-        let (socket, _) = listner.accept().await.unwrap();
-        tokio::spawn(async move { process(socket).await });
-    }
-}
-
-async fn process(socket: TcpStream) {
-    // hashmap for the key value store
-    let mut kv_store = HashMap::new();
-
+async fn process(socket: TcpStream, db: ShardedDb) {
     let mut connection = Connection::new(socket);
 
     // use read_frame to receive a command from the connection
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                // The value is stored as `Vec<u8>`
-                kv_store.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
-                if let Some(value) = kv_store.get(cmd.key()) {
+                let db = db.lock().unwrap();
+                if let Some(value) = db.get(cmd.key()) {
                     // `Frame::Bulk` expects data to be of type `Bytes`.
                     // `&Vec<u8>` is coverted to `Bytes` using `into()`
                     Frame::Bulk(value.clone().into())
@@ -41,5 +33,23 @@ async fn process(socket: TcpStream) {
         };
 
         connection.write_frame(&response).await.unwrap();
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let listner = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    println!("Listening on: 127.0.0.1:6379");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
+    loop {
+        let (socket, _) = listner.accept().await.unwrap();
+        let db = db.clone();
+
+        println!("Accepted");
+
+        tokio::spawn(async move { process(socket, db).await });
     }
 }
